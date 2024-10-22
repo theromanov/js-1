80b2aaf5e97a318c37701370866eb4305097e60d
Roman <programistroman@gmail.com>
2024-10-22 17:04:33 +0100

    mini-programm

--- js/script.js
+++ js/script.js
@@ -1,26 +1,25 @@
 "use strict";
 
-const numberOfFilms = +prompt('Скільки фільмів ви уже подивилися?', '');
+const numberOfFilms = +prompt('Скільки ви переглянли фільмів?');
 
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
-    geners: [],
+    genres: [],
     privat: false
 };
 
 
-const a = prompt('Один з останніх переглянутих фільмів?', ''),
-    b = prompt('На скільки оціните його?', ''),
-    c = prompt('Один з останніх переглянутих фільмів?', ''),
-    d = prompt('На скільки оціните його?', ''),
-    e = prompt('Які Ваші улюблені актори?'),
-    f = prompt('На скільки б оцінили їхні вміння?')
-
+const a = prompt('Один з останніх переглянутих фільмів?'),
+    b = +prompt('На скільки оцінити його?'),
+    c = prompt('Один з останніх переглянутих фільмів?'),
+    d = +prompt('На скільки оцінити його?');
 
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
-personalMovieDB.actors[e] = f;
 
-console.log(personalMovieDB);
+
+console.log(personalMovieDB)
+
+
