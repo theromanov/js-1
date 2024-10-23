6bf6bbfa89413d9a0b09ca65029b4ebcbd3d9947
Roman <programistroman@gmail.com>
2024-10-23 10:06:18 +0100

    easy task

--- js/script.js
+++ js/script.js
@@ -1,51 +1,50 @@
 "use strict";
 
-const numberOfFilms = +prompt('Скільки ви переглянли фільмів?');
+// const numberOfFilms = +prompt('Скільки ви переглянли фільмів?');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-
-const a = prompt('Один з останніх переглянутих фільмів?'),
-    b = +prompt('На скільки оцінити його?'),
-    c = prompt('Один з останніх переглянутих фільмів?'),
-    d = +prompt('На скільки оцінити його?');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
 
-console.log(personalMovieDB);
+// const a = prompt('Один з останніх переглянутих фільмів?'),
+//     b = +prompt('На скільки оцінити його?'),
+//     c = prompt('Один з останніх переглянутих фільмів?'),
+//     d = +prompt('На скільки оцінити його?');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+
+// console.log(personalMovieDB);
 
 
 
-if (num < 49) {
-    console.log('Брехня')
-} else if (num > 100) {
-    console.log('Забагато!')
-} else if (num === 50) {
-    console.log('Перемога!')
+// if (num < 49) {
+//     console.log('Брехня')
+// } else if (num > 100) {
+//     console.log('Забагато!')
+// } else if (num === 50) {
+//     console.log('Перемога!')
+// }
+
+
+
+let result = '';
+
+for (let i = 0; i <= 5; i++) {
+    for (let j = 0; j < 5 - i; j++) {
+        result += " ";
+    }
+    for (let j = 0; j < 2 * i + 1; j++) {
+        result += "*";
+    }
+    result += "\n";
 }
 
-const num = 50;
-
-switch (num = 50) {
-    case 49:
-        console.log('не вірно');
-        break;
-    case 100:
-        console.log('Забагато!');
-        break;
-    case 51:
-        console.log('Все правильно!');
-        break;
-    default:
-        console.log('Не в цей раз!');
-        break;
-}
+console.log(result);
 
